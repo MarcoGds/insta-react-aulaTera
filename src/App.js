053406991c90b2ca3ef8024d426bd16e6058e7c8
MarcoGds <marco_guedes@outlook.com>
2022-04-04 18:14:50 -0300

    Primeiro commit
    Cópia do Instagram da aula sobre React da Tera

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './CSS/normalize.css';
+import './CSS/main.css';
+import './CSS/fontawesome.min.css';
+
+import { Topbar, Stories, Feed } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <React.Fragment>
+
+  <Topbar />
+
+  <Stories />
+
+  <Feed />
+
+  </React.Fragment>
+ 
   );
 }
 
